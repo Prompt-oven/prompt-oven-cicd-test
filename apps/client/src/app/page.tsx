3d2c81b611f6ac9b2fa53f191dcdbd5541818818
ParkSinGyu <singyu13@gmail.com>
2024-10-23 08:25:52 +0900

    Feat: 모노레포 초기 설정 9 - client앱 아토믹 디자인으로 폴더구조 변경

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -46,10 +46,7 @@ export default function Page(): JSX.Element {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<h1>Home</h1>
-			<p className="fixed left-0 top-0 flex w-full justify-center border-b border-neutral-800 bg-zinc-800/30 bg-gradient-to-b from-inherit px-4 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:bg-zinc-800/30 lg:p-4">
-				examples/with-tailwind -&nbsp;
-				<code className="font-mono font-bold">docs</code>
-			</p>
+			<div className="fixed left-0 top-0 flex w-full justify-center border-b border-neutral-800 bg-zinc-800/30 bg-gradient-to-b from-inherit px-4 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:bg-zinc-800/30 lg:p-4"></div>
 			<div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
 				{LINKS.map(({ title, description }) => (
 					<Card key={title} title={title}>
