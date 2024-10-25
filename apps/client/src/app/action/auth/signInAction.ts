facc396a2a7f87f4418cd96a8712f6368b5efc88
GwendolyNM <qjsduddl12@gmail.com>
2024-10-25 22:22:15 +0900

    Feat: signInAction 수정

--- apps/client/src/app/action/auth/signInAction.ts
+++ apps/client/src/app/action/auth/signInAction.ts
@@ -1,14 +1,14 @@
-'use server';
+"use server"
 
-import { loginSchemaType } from '../../app/schema/authSchema';
+import { loginSchemaType } from "../../../lib/schema/authSchema"
 
 export async function signInAction(data: loginSchemaType): Promise<Response> {
-  return await fetch(``, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-    cache: 'no-cache',
-  });
+	return await fetch(``, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+		cache: "no-cache",
+	})
 }
