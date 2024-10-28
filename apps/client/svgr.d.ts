f291b794191b9e12c0b09936a67a9656ddd21421
ParkSinGyu <singyu13@gmail.com>
2024-10-28 14:13:15 +0900

    JYXB-124/CHORE: svgr.d.ts, tsconfig.json 내에서 svgr와 관련된 설정 추가

--- apps/client/svgr.d.ts
+++ apps/client/svgr.d.ts
@@ -1,11 +1,12 @@
 declare module "*.svg" {
-	import { FC, SVGProps } from "react"
+	import type { FC, SVGProps } from "react"
+
 	const content: FC<SVGProps<SVGElement>>
 	export default content
 }
 
 declare module "*.svg?url" {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any -- any is used here to avoid circular dependency
 	const content: any
 	export default content
 }
