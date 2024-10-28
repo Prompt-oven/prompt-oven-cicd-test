3aa449ea4283f05440a524fbb74030542113f0de
ParkSinGyu <singyu13@gmail.com>
2024-10-28 17:48:47 +0900

    JYXB-123/FEA: MainFooter.tsx 컴포넌트 생성, Footer에 필요한 아이콘 생성

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,9 +1,12 @@
 import MainHeader from "@/components/main/molecule/MainHeader.tsx"
+import MainFooter from "@/components/main/molecule/MainFooter.tsx"
 
 export default function Page() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between">
 			<MainHeader />
+
+			<MainFooter />
 		</main>
 	)
 }
