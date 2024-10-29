658776c10b41fbf19bd221e13e851f6da3e3cae4
ParkSinGyu <singyu13@gmail.com>
2024-10-29 15:00:44 +0900

    JYXB-121/FEA: CategoryList.tsx 컴포넌트 생성 및 page.tsx에 반영

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,13 +1,48 @@
 import MainHeader from "@/components/main/molecule/MainHeader.tsx"
 import MainFooter from "@/components/main/organism/MainFooter.tsx"
 import ImageCarousel from "@/components/main/molecule/ImageCarousel.tsx"
+import CategoryList from "@/components/main/molecule/CategoryList.tsx"
+
+const promptCategories = [
+	{
+		title: "3D Art",
+		images: ["/img/main/art1.png", "/img/main/art2.png", "/img/main/art3.png"],
+	},
+	{
+		title: "Digital Painting",
+		images: [
+			"/img/main/collectibles1.png",
+			"/img/main/collectibles2.png",
+			"/img/main/collectibles3.png",
+		],
+	},
+	{
+		title: "Stylized Art",
+		images: ["/img/main/art1.png", "/img/main/art2.png", "/img/main/art3.png"],
+	},
+	{
+		title: "Realistic Art",
+		images: ["/img/main/art1.png", "/img/main/art2.png", "/img/main/art3.png"],
+	},
+	{
+		title: "Music",
+		images: ["/img/main/art1.png", "/img/main/art2.png", "/img/main/art3.png"],
+	},
+	{
+		title: "Photography",
+		images: ["/img/main/art1.png", "/img/main/art2.png", "/img/main/art3.png"],
+	},
+]
 
 export default function Page() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between">
+		<main className="flex min-h-screen flex-col items-center justify-between bg-[#111111]">
 			<MainHeader />
-      <div className="flex h-[1000px] w-full items-center justify-center p-8">
-			  <ImageCarousel />
+			<div className="flex w-full flex-col items-center justify-center gap-20 p-8">
+				<ImageCarousel />
+				<div className="py-10">
+					<CategoryList categories={promptCategories} />
+				</div>
 			</div>
 			<MainFooter />
 		</main>
