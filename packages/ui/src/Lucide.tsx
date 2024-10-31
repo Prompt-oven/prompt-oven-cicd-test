c5febcf7f986dbb7f7d176064cbb37d8e943cf35
subin <osb4469@gmail.com>
2024-10-31 17:20:10 +0900

    merge conflict resolving

--- packages/ui/src/Lucide.tsx
+++ packages/ui/src/Lucide.tsx
@@ -1,5 +1,9 @@
-<<<<<<< HEAD
-export { User, Search, ChevronLeft, ChevronRight, ArrowRight, ChevronDown, X } from "lucide-react";
-=======
-export { User, Search, ChevronLeft, ChevronRight, ChevronDown ,ArrowRight } from "lucide-react";
->>>>>>> 6742fcdbe28fd0c960249e1c3e3a959f1766f9d6
+export {
+  User,
+  Search,
+  ChevronLeft,
+  ChevronRight,
+  ChevronDown,
+  ArrowRight,
+  X,
+} from "lucide-react";
