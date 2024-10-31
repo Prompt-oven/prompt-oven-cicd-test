e6379146436149c07e00588fdd09c6aae625782e
ParkSinGyu <singyu13@gmail.com>
2024-10-31 10:33:39 +0900

    JYXB-176/UPDATE: ImageCarousel.tsx을 감싸는 PromptImageCarousel.tsx 컴포넌트 생성
    content:

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,15 +1,14 @@
-import { Button } from "@repo/ui/button"
 import FeatureDescription from "@/components/main/atom/FeatureDescription.tsx"
 import AccountSvg from "@/components/main/atom/icon/AccountSvg.tsx"
 import AddFolderSvg from "@/components/main/atom/icon/AddFolderSvg.tsx"
 import AddPromptSvg from "@/components/main/atom/icon/AddPromptSvg.tsx"
 import SaleSvg from "@/components/main/atom/icon/SaleSvg.tsx"
 import FeatureDescriptionContainer from "@/components/main/atom/FeatureDescriptionContainer.tsx"
-import ImageCarousel from "@/components/main/molecule/ImageCarousel.tsx"
 import CategoryList from "@/components/main/molecule/CategoryList.tsx"
 import NotableDropsCarousel from "@/components/main/organism/NotableDropsCarousel.tsx"
 import MainFooter from "@/components/main/organism/MainFooter.tsx"
 import BestSellerFilter from "@/components/main/organism/BestSellerFilter.tsx"
+import PromptImageCarousel from "@/components/main/organism/PromptImageCarousel.tsx"
 
 const steps = [
 	{
@@ -131,37 +130,49 @@ const sellers = [
 	{ number: 15, nickname: "Creator 15", earnings: 16143658, isVerified: true },
 ]
 
+const images = [
+	{
+		src: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/14d64d25a42a191fc83ed8fe0131d55e-elpi9POXTCICMSLKPSWwoBsGyvbQOw.png",
+		alt: "Cute cartoon character in bear costume with smaller bear",
+		title: "Bear Friends",
+		creator: "@bearartist",
+		mainDesc: "1 - Eva Sheppard | AI https://t.me/neirolapki",
+		subDesc:
+			"1 - Ai Prompts Для заказов и сотрудничества ⬇️ @Eva_Sheppard ⚡Промты для нейросетей⚡ #Midjourney prompts. #Шедеврум промты. Stable Diffusion. Канал для вдохновения 💜 #Midjourney ⬇️ http://clck.ru/3BCUnQ",
+	},
+	{
+		src: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/b1a6d4c50df289b2a6a9a07124702274-4jydo2FU8fWjY5BCng03SGBGMSasEi.png",
+		alt: "Close-up of cartoon character in bear costume",
+		title: "크양",
+		creator: "@koreanartist",
+		mainDesc: "2 - Eva Sheppard | AI https://t.me/neirolapki",
+		subDesc:
+			"2 - Ai Prompts Для заказов и сотрудничества ⬇️ @Eva_Sheppard ⚡Промты для нейросетей⚡ #Midjourney prompts. #Шедеврум промты. Stable Diffusion. Канал для вдохновения 💜 #Midjourney ⬇️ http://clck.ru/3BCUnQ",
+	},
+	{
+		src: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/a94f15e50a0469c3be2ddb4899f7c942-1QwjkrzSSCeKm5ZacaXZQReGQfNgiS.png",
+		alt: "Anime girl with pink hair taking selfie in magical room",
+		title: "Magical Selfie",
+		creator: "@animeartist",
+		mainDesc: "3 - Eva Sheppard | AI https://t.me/neirolapki",
+		subDesc:
+			"3 - Ai Prompts Для заказов и сотрудничества ⬇️ @Eva_Sheppard ⚡Промты для нейросетей⚡ #Midjourney prompts. #Шедеврум промты. Stable Diffusion. Канал для вдохновения 💜 #Midjourney ⬇️ http://clck.ru/3BCUnQ",
+	},
+	{
+		src: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/%E1%84%92%E1%85%A9%E1%84%80%E1%85%AE%E1%84%8B%E1%85%AA%E1%84%90%E1%85%B3-%E1%84%8E%E1%85%A1%E1%86%B8-cZ48jAqFyuoVQcABL0KztIIBFgS4yi.png",
+		alt: "Anime girl with pink hair by window on train",
+		title: "Magical Journey",
+		creator: "@abstractart",
+		mainDesc: "4 - Eva Sheppard | AI https://t.me/neirolapki",
+		subDesc:
+			"4 - Ai Prompts Для заказов и сотрудничества ⬇️ @Eva_Sheppard ⚡Промты для нейросетей⚡ #Midjourney prompts. #Шедеврум промты. Stable Diffusion. Канал для вдохновения 💜 #Midjourney ⬇️ http://clck.ru/3BCUnQ",
+	},
+]
+
 export default function Page() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between bg-[#111111]">
-			<div className="flex w-full items-center justify-center gap-[100px] py-40">
-				<ImageCarousel />
-
-				{/* todo - 이미지 카로셀과 연관된 데이터가 필요하므로 ImageCarousel 컴포넌트와 같이 구성된 새로운 컴포넌트 또는 해당 컴포넌트로 옮길 필요가 있음 */}
-				<div className=" relative max-w-[724px]">
-					<h1 className="mb-6 font-sora text-[55px] font-semibold leading-[64px] text-white">
-						Eva Sheppard | AI https://t.me/neirolapki
-					</h1>
-
-					<p className="mb-8 font-sora text-[19px] leading-[28px] text-[#969696]">
-						Ai Prompts Для заказов и сотрудничества ⬇️ @Eva_Sheppard ⚡Промты
-						для нейросетей⚡ #Midjourney prompts. #Шедеврум промты. Stable
-						Diffusion. Канал для вдохновения 💜 #Midjourney ⬇️
-						http://clck.ru/3BCUnQ
-					</p>
-
-					<div className="flex gap-4">
-						<Button className="h-[50px] w-[152px] bg-gradient-to-r from-[#A913F9] to-[#3F5EFB] font-sora text-[15px] font-semibold">
-							ADD CART
-						</Button>
-						<Button
-							variant="outline"
-							className="h-[50px] w-[118px] border-[#424242] bg-[#111111] font-sora text-[15px] font-semibold text-white">
-							Share
-						</Button>
-					</div>
-				</div>
-			</div>
+			<PromptImageCarousel images={images} />
 
 			<div className="w-full py-32">
 				<NotableDropsCarousel items={notableDrops} />
