24270aa2a04a83ececf6ce7ef2c26e6f7e82e104
PARK SINGYU <singyu13@gmail.com>
2024-11-15 15:17:53 +0900

    Update next.config.js

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	env: {
-		AWS_REGION: process.env.AWS_REGION,
-		BUCKET: process.env.BUCKET,
-		AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-		AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-		GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-		GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-		NAVER_CLIENT_ID: process.env.NAVER_CLIENT_ID,
-		NAVER_CLIENT_SECRET: process.env.NAVER_CLIENT_SECRET,
-		KAKAO_CLIENT_ID: process.env.KAKAO_CLIENT_ID,
-		KAKAO_CLIENT_SECRET: process.env.KAKAO_CLIENT_SECRET,
-	},
 	reactStrictMode: true,
 	transpilePackages: ["@repo/ui"],
 	images: {
